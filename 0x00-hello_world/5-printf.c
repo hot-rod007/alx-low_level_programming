333d72b18261ac20ba23001232b02c1f334c3141
hot -rod007 <emmanuelhermanc@outlook.com>
2022-12-08 18:43:57 -0800

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,16 @@
 
 /**
 *main - Print a string 
+*
 *Description: use printf
+*
 *Return:0
 */
 
 int main(void)
 {
+
         printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }	
