9f473839c88d42d14bd5e9b7c4a1e43ec059c1cb
hot -rod007 <emmanuelhermanc@outlook.com>
2022-12-08 18:57:42 -0800

    Phello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
-*main - Print a string 
-*
-*Description: use printf
-*
-*Return:0
-*/
+ * main - Print a string
+ *
+ * Description: use printf
+ *
+ * Return: 0
+ */
 
 int main(void)
+
 {
 
-        printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
-}	
+}
